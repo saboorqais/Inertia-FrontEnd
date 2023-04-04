0a5712b5ae95aef1194a503ccac286ca1d39618b
saboorqais <saboor.qaiser456@gmail.com>
2023-04-04 11:29:09 +0500

    Added Heading

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import { HStack, Heading, VStack ,Text} from 'native-base'
 
 const Cart = () => {
-  return (
-    <View>
-      <Text>Cart</Text>
-    </View>
-  )
+    return (
+        <VStack flex={1} style={{
+            backgroundColor: 'white',
+        }}>
+            <HStack ml={5} mr={5} mt={5} justifyContent={"space-between"}>
+                <Heading style={{ color: 'black' }}>Items</Heading>
+                <Text style={{ color: 'black' }}>2</Text>
+            </HStack>
+        </VStack>
+    )
 }
 
 export default Cart
